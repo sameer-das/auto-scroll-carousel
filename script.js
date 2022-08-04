9ec8daa9b6d61fd7420bcd9da180c5bd577083a1
sameer-das <playsameer979@gmail.com>
2022-08-04 17:40:50 +0530

    cleanup and added hover effect

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 function carousel() {
     const carouselSlider = document.querySelector('.carousel_slider');
     const list = document.querySelector('.carousel_list');
-    const items = document.querySelectorAll('.carousel_item');
     console.log(list.offsetWidth)
     let list2;
 
-    let width =  list.offsetWidth;
+    let width = list.offsetWidth;
     let x = 0;
     let x2 = width;
     let speed = 1;
+    let a;
+    let b;
     function clone() {
-         list2 = list.cloneNode(true);
-         carouselSlider.appendChild(list2);
-         list2.style.left = `${width}px`
+        list2 = list.cloneNode(true);
+        carouselSlider.appendChild(list2);
+        list2.style.left = `${width}px`
     }
 
-    function moveFirst(){
+    function moveFirst() {
         x = x - speed;
-        if(width >= Math.abs(x)) {
-            list.style.left =  `${x}px`;
-        }else {
+        if (width >= Math.abs(x)) {
+            list.style.left = `${x}px`;
+        } else {
             x = width;
         }
     }
 
-    function moveSecond(){
+    function moveSecond() {
         x2 = x2 - speed;
-        if(list2.offsetWidth >= Math.abs(x2)) {
-            list2.style.left =  `${x2}px`;
-        }else {
+        if (list2.offsetWidth >= Math.abs(x2)) {
+            list2.style.left = `${x2}px`;
+        } else {
             x2 = width;
         }
     }
 
-    clone();
+    
+    function startScroll() {
+        a = setInterval(moveFirst, 20);
+        b = setInterval(moveSecond, 20);
+    }
 
-    let a = setInterval(moveFirst, 20);
-    let b = setInterval(moveSecond, 20);
+    function stopScroll() {
+        clearInterval(a);
+        clearInterval(b);
+    }
+
+    clone();
+    startScroll();
+
+    carouselSlider.addEventListener('mouseenter', () => {
+       stopScroll()
+    });
+
+    carouselSlider.addEventListener('mouseleave', () => {
+        startScroll()
+    })
+
 }
 
 carousel();
